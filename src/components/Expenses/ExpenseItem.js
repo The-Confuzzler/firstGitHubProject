67469f9a5c441c7f7b85f24495e170aef7274263
Tucker <tuckerd888@gmail.com>
2021-09-09 09:06:08 -0400

    next module

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,14 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
     // const expenseDate  = Date(2021, 8, 24);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.67;
-    const clickHandler = () => {console.log('clicked!\n')};
+   
+    const clickHandler = () => {setTitle("Updated!");};
    
   return (
       //the top div is called a wrapping div
@@ -13,7 +16,7 @@ function ExpenseItem(props) {
      <ExpenseDate date={props.date} year={props.year} month={props.year}/>
      {/* {ExpenseDate(props)} */}
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
